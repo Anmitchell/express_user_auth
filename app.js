b19a69dc4c238b7306ba725a249faeb3cc5cb1f9
Avary Mitchell <avarynmitchell@gmail.com>
2025-01-21 22:29:28 -0500

    Adding all files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectDB = require("./config/database");
+const authRoutes = require("./routes/authRoutes");
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json()); // Parse incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: false })); // Parse incoming requests with URL-encoded payloads (like form submissions).
+
+// Connect to the database
+connectDB();
+
+// Use authentication routes
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  // Using regular function here
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare hashed password with input
+userSchema.methods.comparePassword = function (candidatePassword) {
+  // Regular function for methods as well
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
